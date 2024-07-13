d71eb1a55cfc16eb885db8b2b7334488526823b5
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-13 23:38:47 +0530

    Complete File structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+function App() {
+  return (
+    <View>
+      <Text>Hello world haha</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
